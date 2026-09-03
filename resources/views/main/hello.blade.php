4e2c000bf31347a302d7328325d41267d3b55ef5
root <viepurple@yandex.ru>
2026-09-03 21:43:26 +0300

    maincontroller

--- resources/views/main/hello.blade.php
+++ resources/views/main/hello.blade.php
@@ -1,4 +1,25 @@
 @extends('layout')
 @section('content')
-<h2>Hello! I'm youre first lavarel project!</h2>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Date</th>
+      <th scope="col">Name</th>
+      <th scope="col">SortDesc</th>
+      <th scope="col">Desc</th>
+      <th scope="col">Image</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach($articles as $article)
+    <tr>
+      <th scope="row">{{$article['date']}}</th>
+      <td> {{$article['name']}}</td>
+      <td> {{$article['shortDesc']}}</td>
+      <td> {{$article['desc']}}</td>
+      <td> {{$article['preview_image']}}</td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
 @endsection
