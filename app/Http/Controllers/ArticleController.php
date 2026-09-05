50485aeed2fc3b0c8759d929e41c56ba006744af
root <viepurple@yandex.ru>
2026-09-06 02:24:01 +0300

    LR5

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -15,7 +15,7 @@ class ArticleController extends Controller
     public function index()
     {
         $articles=Article::all();
-        return view('articles/show', ['articles'=>$articles]);
+        return view('articles/index', ['articles' => $articles]);
     }
 
     /**
