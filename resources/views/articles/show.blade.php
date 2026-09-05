73c005b79e6082ab304f022abdb7f0fa00699169
root <viepurple@yandex.ru>
2026-09-06 00:10:38 +0300

    LR5

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,29 +1,5 @@
 @extends('layout')
 @section('content')
-<table class="table">
-  <thead>
-    <tr>
-      <th scope="col">Date</th>
-      <th scope="col">Title</th>
-      <th scope="col">shortDesc</th>
-      <th scope="col">Desc</th>
-    </tr>
-  </thead>
-  <tbody>
-@foreach($articles as $article)
-    <tr>
-      <th scope="row">{{$article['datePublic']}}</th>
-      <td><a href="/article/{{$article->id}}">>{{$article['title']}}</a</td>
-      <td>{{$article['shortDesc']}}</td>
-      <td>{{$article['desc']}}</td>
-    </tr>
-@endforeach
-  </tbody>
-</table>
-@endsection
-
-@extends('layout')
-@section('content')
 <div class="card" style="width: 18rem;">
   <div class="card-body">
     <h5 class="card-title">{{$article->title}}</h5>
